f5cd412b1e8124b823c13a2ee22993eada3b8b0e
Tiago Hermano <tiagofhermano@gmail.com>
2018-04-20 13:15:37 -0300

    Excluído arquivo desnecessário

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ var fs = require('fs');
 var path = require('path');
 
 function encrypt(pubKey, text) {
-  // Encrypt with the public key...
   var encrypt = new JSEncrypt();
   encrypt.setPublicKey(pubKey);
   return encrypt.encrypt(text);
 }
 
 function decrypt(privKey, encryptedText) {
-  // Decrypt with the private key...
   var decrypt = new JSEncrypt();
   decrypt.setPrivateKey(privKey);
   return decrypt.decrypt(encryptedText);
